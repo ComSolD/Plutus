7b8e4b2fc71b8232ed09e873cd36378e44e48c10
ComSolD <2dcsol5@gmail.com>
2025-01-13 11:49:35 +0300

    Исправление базы данных

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from parser.NBA.parser import ParsingNBA
 
 
 if __name__ == "__main__":
-    ParsingNBA('2025-01-10','2025-01-10').date_cycle()
+    ParsingNBA('2025-01-14','2025-01-14').date_cycle()
